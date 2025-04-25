68af7d36f64ddf030c27e5c0824924cdea27878f
lukaholota <lukagolota1@gmail.com>
2025-04-25 13:40:06 +0300

    flake8

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from .chat import Chat
-from .user import User
-from .chat_participant import ChatParticipant
-from .message import Message
+from .chat import Chat  # noqa: F401
+from .user import User  # noqa: F401
+from .chat_participant import ChatParticipant  # noqa: F401
+from .message import Message  # noqa: F401
